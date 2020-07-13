8d43d90d54872bb45079f5648fa8c55bcbc268b9
StlMaris123 <stellamarisnjage@gmail.com>
2020-07-13 20:16:37 +0200

    Add simple cov for test coverage

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -3,7 +3,8 @@
 class DoctorsController < ApplicationController
 
   def hospitals
-    @client = GooglePlaces::Client.new(ENV['PLACES_API'])
-    @client.spots(@user.latitude, @user&.longitude, :types => 'hospital')
+    # @client = GooglePlaces::Client.new(ENV['PLACES_API'])
+    # @client.spots(@user.latitude, @user&.longitude, :types => 'hospital')
+    @client = User.near(@user.address).where(tags: [:hospital]).all
   end
 end
